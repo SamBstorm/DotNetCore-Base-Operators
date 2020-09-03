c1b86ebe415a45c8f816efd637aec59d683c2ad3
Samuel Legrain <samuel.legrain@bstorm.be>
2020-09-03 14:56:55 +0200

    Exo_1 - Division, Modulo et Division entière de 2 entiers

--- Exo_Operators/Program.cs
+++ Exo_Operators/Program.cs
@@ -6,7 +6,15 @@ namespace Exo_Operators
     {
         static void Main(string[] args)
         {
-            
+            #region Exo_1 - Division, modulo, division entière sur deux entiers
+
+            //int nb1, nb2;
+            //nb1 = 5;
+            //nb2 = 2;
+            //Console.WriteLine($"Division entière : {nb1/nb2} - Modulo : {nb1%nb2} - Division : {(double)nb1/nb2}");
+
+            #endregion
+
         }
     }
 }

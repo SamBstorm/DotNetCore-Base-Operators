46ce82986cbb86d92a931c894d181dab844c55f8
Samuel Legrain <samuel.legrain@bstorm.be>
2020-09-03 15:32:20 +0200

    Exo_3 - Conversion BBAN to IBAN

--- Exo_Operators/Program.cs
+++ Exo_Operators/Program.cs
@@ -17,24 +17,41 @@ namespace Exo_Operators
 
             #region Exo_2 - Vérification du BBAN
 
+            //Console.WriteLine("Veuillez introduire votre BBAN:");
+            //string bban = Console.ReadLine();
+            //if (ulong.TryParse(bban, out _) && bban.Length == 12)
+            //{
+            //    ulong tenFirst = ulong.Parse(bban.Substring(0, 10));            //tenFirst = bban / 100 => Division entière
+            //    ushort twoLast = ushort.Parse(bban.Substring(10,2));            //twoLast = bban % 100 => Reste inférieur à 100
+            //    if ((tenFirst%97 == twoLast)||(tenFirst%97==0 && twoLast == 97))
+            //    {
+            //        Console.WriteLine("OK");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("KO");
+            //    }
+
+            //}
+            //else {
+            //    Console.WriteLine("Votre BBAN n'est pas valide.");
+            //}
+
+            #endregion
+
+            #region Exo_3 - Conversion BBAN to IBAN
+
             Console.WriteLine("Veuillez introduire votre BBAN:");
             string bban = Console.ReadLine();
-            if (ulong.TryParse(bban, out _) && bban.Length == 12)
+            
+            if(ulong.TryParse(bban, out _) && bban.Length == 12)
             {
-                ulong tenFirst = ulong.Parse(bban.Substring(0, 10));            //tenFirst = bban / 100 => Division entière
-                ushort twoLast = ushort.Parse(bban.Substring(10,2));            //twoLast = bban % 100 => Reste inférieur à 100
-                if ((tenFirst%97 == twoLast)||(tenFirst%97==0 && twoLast == 97))
-                {
-                    Console.WriteLine("OK");
-                }
-                else
-                {
-                    Console.WriteLine("KO");
-                }
+                string bban2 = bban + "111400";
+                long convert = long.Parse(bban2);
+                short position = (short)(98 - (convert % 97));
 
-            }
-            else {
-                Console.WriteLine("Votre BBAN n'est pas valide.");
+                Console.WriteLine($"Votre IBAN est BE{position:D2}{bban}");
+                //Console.WriteLine("Votre IBAN est BE"+position.ToString("00")+bban);
             }
 
             #endregion
